9dcdc01854e8176d6847cab95952701adbad9e0c
Hyun Park <nemod.leo@snu.ac.kr>
2022-11-06 14:07:22 +0000

    pip install 0.0.2

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # pytorch-histogram-matching
 
+## Installation
+```bash
+pip install pytorch_histogram_matching
+```
+
+## Usage
+```python
+from pytorch_histogram_matching import Histogram_Matching
+
+import torch
+dst = torch.randint(0, 256, (4, 3, 512,512)).cuda() / 255.
+ref = torch.randint(0, 256, (4, 3, 512,512)).cuda() / 255.
+
+HM = Histogram_Matching()
+rst = HM(dst, ref)
+```
+
 ## Test
 ```bash
 python test.py
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+version = '0.0.2'
+
+setuptools.setup(
+    name='pytorch_histogram_matching',
+    version=version,
+    url='https://github.com/nemodleo/pytorch-histogram-matching',
+    author='Hyun Park',
+    author_email='nemod.leo@snu.ac.kr',
+    license="MIT",
+    description='pytorch implementation of histogram matching',
+    keywords="hm color pytorch histogram matching",
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+    python_requires=">= 3.6",
+    packages=['pytorch_histogram_matching'],
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+    package_data={},
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ] 
+)
